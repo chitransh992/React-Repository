38c510f7ef95b8b304405c501b5919e35f71f8f9
chitransh992 <105778129+chitransh992@users.noreply.github.com>
2023-10-17 16:29:56 +0530

    changes

--- 01viteexample/src/main.jsx
+++ 01viteexample/src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+
+function MyApp(){
+  const username  = "Chitransh"
+  return(
+    <div>
+      <h1>Custom App | {username}</h1>
+    </div>
+  )
+}
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <MyApp/>
+    // <App />
 )
